85b65e7de234335c26980628bbc03396266beb6e
wanmuhafidzfaldi <wanmuhafidzfaldi@gnail.com>
2020-01-12 10:55:22 +0700

    add example azure

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dicoding-server-1.database.windows.net";
+    $user = "dicoding-server-1";
+    $pass = "Wanfaldi24";
+    $db = "dicoding_submission_1";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
